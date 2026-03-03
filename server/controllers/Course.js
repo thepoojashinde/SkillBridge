656ce2f86b29680a561b46e57b6ec8aa5de91c00
Pooja Shinde <poojashindeps213@gmail.com>
2026-03-03 23:38:59 +0530

    Fix model case issue 2

--- server/controllers/Course.js
+++ server/controllers/Course.js
@@ -1,5 +1,5 @@
 const Course = require("../models/Course")
-const Category = require("../models/category")
+const Category = require("../models/Category")
 const Section = require("../models/Section")
 const SubSection = require("../models/SubSection")
 const User = require("../models/user")
